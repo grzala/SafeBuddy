61fd4a872294db7c090714b77526ec1b3fd65d42
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2015-11-19 18:51:14 +0000

    pie charts selection

--- lib/helper_csv.rb
+++ lib/helper_csv.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 def upload_police_crimes
 	path = "vendor/police_crime/"
-	years = [2014]
+	years = [2014, 2013, 2012, 2011, 2010, 2009]
 
 	years.each do |year|
 		file = path + year.to_s + ".csv"
@@ -18,6 +18,9 @@ def upload_police_crimes
 					while(@region[-1] == " ")
 						@region = @region.chop
 					end
+					while(@region[0] == " ")
+						@region = @region.reverse.chop.reverse
+					end
 				else
 				@crime = PoliceRegionCrime.new
 				@crime.year = year
@@ -26,6 +29,9 @@ def upload_police_crimes
 				#delete whitespace
 				while(key[-1] == " ")
 					key = key.chop
+				end
+				while(key[0] == " ")
+					key = key.reverse.chop.reverse
 				end
 				
 				puts "category: "+key+" number: "+value.to_s+" year: "+year_string+" region: "+@region
